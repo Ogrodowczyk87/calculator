353a8ea875136ba609b421ed7f36edc7e68cb88e
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2024-02-27 20:44:36 +0000

    m

---
+++ script.js
@@ -0,0 +1,26 @@
+let displayValue = "0";
+function updateDisplay() {
+    document.getElementById("display").textContent = displayValue;
+}
+
+function appendToDisplay(value) {
+    if (displayValue === "0") {
+        displayValue = value
+    } else {
+        displayValue += value
+    }
+    updateDisplay()
+}
+
+function calculateResult(){
+    try {
+        displayValue=eva(displayValue).toString()
+    } catch(error){
+        displayValue="Error"
+    }
+    updateDisplay()
+}
+function clearDisplay() {
+    displayValue = "0"
+    updateDisplay()
+}
